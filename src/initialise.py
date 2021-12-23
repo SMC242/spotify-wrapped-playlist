33ae506be5fc2e751dcb4eacf53dfa9625ff3b1c
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-12-23 09:36:59 +0000

    Graceful exit on no secrets file

--- src/initialise.py
+++ src/initialise.py
@@ -1,11 +1,13 @@
 from json import load
 import aiohttp
 from typing import Tuple
+from os import PathLike
+from pathlib import Path
 
 from src.requester import SpotifyRequester
 
 
-def load_secrets(path: str) -> Tuple[str, str]:
+def load_secrets(path: PathLike) -> Tuple[str, str]:
     """Get the client id and secret"""
     with open(path) as f:
         json = load(f)
@@ -17,8 +19,11 @@ def get_requester() -> SpotifyRequester:
     Create a SpotifyRequester.
     You must remember to call requester.session.close() when you're done.
     """
-    client_id, client_secret = load_secrets(
-        "playlist_stats/secrets.json")
+    try:
+        client_id, client_secret = load_secrets(
+            Path("secrets.json"))
+    except FileNotFoundError:
+        raise RuntimeError("No secrets file found")
     session = aiohttp.ClientSession()
     requester = SpotifyRequester(
         client_id, client_secret, session)
