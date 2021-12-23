76dc7523d42395cbe9f992a0970f61bce62b3e21
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-12-23 09:12:51 +0000

    Corrected typing error

--- src/plot.py
+++ src/plot.py
@@ -1,6 +1,6 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-from typing import List, Tuple, Any
+from typing import List, Tuple, Any, Counter as CounterT
 from collections import Counter
 import toolz.curried as toolz
 
@@ -8,7 +8,7 @@ from src.songs import parse_tracks
 from src.genres import all_genres
 
 
-def plot_years(counter: Counter[int]) -> None:
+def plot_years(counter: CounterT[int]) -> None:
     breakdown = {
         "Year": list(counter.keys()),
         "Number of songs": list(counter.values()),
