4fc5365c23403a6297f360ee30dc5c0884c530c3
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-12-29 02:23:26 +0000

    Refactored songs to be more terse

--- src/songs.py
+++ src/songs.py
@@ -1,35 +1,16 @@
-from typing import Iterator, List
-#from collections.abc import Sequence
 import toolz.curried as toolz
 from datetime import datetime, date
 
-DictIterator = Iterator[dict]
 
-
-def to_added_at(tracks: List[dict]) -> Iterator[str]:
-    """Flatten a list of tracks to their added_at attribute"""
-    return map(toolz.get_in(["added_at"]), tracks)
-
-
-# I couldn't get Sequence to work with mypy and strings
-# so no type :,(
 def drop_last(seq): return seq[:-1]
 
 
-def remove_z(timestamps: Iterator[str]) -> Iterator[str]:
-    return map(drop_last, timestamps)
+to_added_at = toolz.get_in(["added_at"])
+remove_z = drop_last
+parse_datetime = datetime.fromisoformat
+to_date = datetime.date
+def to_year(d: datetime): return d.year
 
 
-def parse_dates(timestamps: Iterator[str]) -> Iterator[datetime]:
-    return map(datetime.fromisoformat, timestamps)
-
-
-def to_dates(xs: Iterator[datetime]) -> Iterator[date]:
-    return map(datetime.date, xs)
-
-
-def to_years(xs: Iterator[date]): return map(lambda d: d.year, xs)
-
-
-parse_tracks = toolz.compose(
-    to_years, to_dates, parse_dates, remove_z, to_added_at)
+parse = toolz.compose(to_year, to_date, parse_datetime, remove_z, to_added_at)
+parse_tracks = toolz.map(parse)
--- tests/test_songs.py
+++ tests/test_songs.py
@@ -1,6 +1,6 @@
 import unittest
 from json import load
-from datetime import date
+from datetime import date, datetime
 from toolz import compose
 
 import src.songs as songs
@@ -12,25 +12,34 @@ class TestSongs(unittest.TestCase):
             self.tracks = load(f)["tracks"]
 
     def test_added_at(self):
-        expected = ['2021-12-23T09:58:36Z',
-                    '2021-12-23T09:58:46Z', '2021-12-23T09:58:51Z']
-        self.assertEqual(expected, list(songs.to_added_at(self.tracks)))
+        expected = '2021-12-23T09:58:36Z'
+        self.assertEqual(expected, songs.to_added_at(self.tracks[0]))
 
-    def test_parse_dates(self):
-        input_ = ['2021-12-23T09:58:36Z',
-                  '2021-12-23T09:58:46Z', '2021-12-23T09:58:51Z']
-        expected = ['2021-12-23', '2021-12-23', '2021-12-23']
-        self.assertEqual(expected, list(songs.parse_dates(input_)))
+    def test_remove_z(self):
+        input_ = '2021-12-23T09:58:36Z'
+        expected = '2021-12-23T09:58:36'
+        self.assertEqual(expected, songs.remove_z(input_))
 
-    def test_to_dates(self):
-        input_ = ['2021-12-23', '2021-12-23', '2021-12-23']
-        expected = [date(2021, 12, 23), date(2021, 12, 23), date(2021, 12, 23)]
-        self.assertEqual(expected, list(songs.to_dates(input_)))
+    def test_parse_date(self):
+        input_ = '2021-12-23T09:58:36'
+        expected = datetime(year=2021, month=12, day=23,
+                            hour=9, minute=58, second=36)
+        self.assertEqual(expected, songs.parse_datetime(input_))
 
-    def test_to_years(self):
-        input_ = [date(2021, 12, 23), date(2021, 12, 23), date(2021, 12, 23)]
+    def test_to_date(self):
+        input_ = datetime(year=2021, month=12, day=23,
+                          hour=9, minute=58, second=36)
+        expected = date(2021, 12, 23)
+        self.assertEqual(expected, songs.to_date(input_))
+
+    def test_to_year(self):
+        input_ = date(2021, 12, 23)
+        expected = 2021
+        self.assertEqual(expected, songs.to_year(input_))
+
+    def test_end2end(self):
         expected = [2021, 2021, 2021]
-        self.assertEqual(expected, list(songs.to_years(input_)))
+        self.assertEqual(expected, list(songs.parse_tracks(self.tracks)))
 
 
 if __name__ == '__main__':
